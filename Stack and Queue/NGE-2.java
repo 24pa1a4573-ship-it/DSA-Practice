baadee6a6d13cbd2961955a6ac0cdddf65c6c312
Karthik_meduri <24pa1a4573@vishnu.edu.in>
2026-07-26 15:42:01 +0530

    Implement nextGreaterElements method in NGE-2.

---
+++ Stack and Queue/NGE-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int[] ans =new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<2*n;i++){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i%n]){
+                ans[st.pop()]=nums[i%n];
+            }
+            if(i<n) st.push(i%n);
+        }
+        while(!st.isEmpty()) ans[st.pop()]=-1;
+        return ans;
+    }
+}
