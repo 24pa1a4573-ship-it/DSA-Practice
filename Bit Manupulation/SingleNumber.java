6dbfea4b36c3431d34ccea8b68d6f9e744a7cec6
Karthik_meduri <24pa1a4573@vishnu.edu.in>
2026-09-03 19:48:07 +0530

    Use of xor Operator, if same 0 otherwise 1
    Implement singleNumber method to find the unique element in an array.

---
+++ Bit Manupulation/SingleNumber.java
@@ -0,0 +1,11 @@
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+//You must implement a solution with a linear runtime complexity and use only constant extra space
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=0;
+        for(int x: nums) n^=x;
+        return n;
+    }
+}
